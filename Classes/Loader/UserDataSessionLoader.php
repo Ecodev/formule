aedd7421ef965c8855e6c92e20482943c26b2337
Fabien Udriot <fabien.udriot@ecodev.ch>
2016-01-28 09:42:40 +0000

    [FEATURE] Introduce user data loader from user session
    Resolves #53

---
+++ Classes/Loader/UserDataSessionLoader.php
@@ -0,0 +1,48 @@
+<?php
+namespace Fab\Formule\Loader;
+
+/**
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+/**
+ * Load user data from session.
+ */
+class UserDataSessionLoader extends AbstractLoader
+{
+
+    /**
+     * @param array $values
+     * @param string $insertOrUpdate
+     * @return array
+     */
+    public function intercept(array $values, $insertOrUpdate = '')
+    {
+        $userData = $this->getFrontendUser()->user;
+
+        if (!empty($userData)) {
+            $values = array_merge($values, $userData);
+        }
+
+        return $values;
+    }
+
+    /**
+     * Returns an instance of the current Frontend User.
+     *
+     * @return \TYPO3\CMS\Frontend\Authentication\FrontendUserAuthentication
+     */
+    protected function getFrontendUser()
+    {
+        return $GLOBALS['TSFE']->fe_user;
+    }
+}
