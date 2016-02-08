3ee6713a25a22b4af573742e5ecf6904d7669dae
Fabien Udriot <fabien.udriot@ecodev.ch>
2016-02-08 12:09:15 +0100

    [FEATURE] Salt password in demo UserDataProcessor
    Resolves #62

--- Classes/Processor/UserDataProcessor.php
+++ Classes/Processor/UserDataProcessor.php
@@ -14,6 +14,9 @@ namespace Fab\Formule\Processor;
  * The TYPO3 project - inspiring people to share!
  */
 
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3\CMS\Saltedpasswords\Salt\SaltFactory;
+use TYPO3\CMS\Saltedpasswords\Utility\SaltedPasswordsUtility;
 
 /**
  * Class UserDataProcessor.
@@ -33,8 +36,8 @@ class UserDataProcessor extends AbstractProcessor
         $values['name'] = $values['first_name'] . ' ' . $values['last_name'];
 
         if ($insertOrUpdate === ProcessorInterface::INSERT) {
-            $values['username'] = ''; // to-do must be controlled as unique
-            $values['password'] = ''; // to-do must be salted
+            $values['username'] = $values['email'];
+            $values['password'] = $this->getSaltedPassword($values['password']);
             #$values['token'] = $this->getUuid(); // fields to be created...
             #$values['is_confirmed'] = 0;
             #$values['is_subscribed'] = 1;
@@ -44,30 +47,47 @@ class UserDataProcessor extends AbstractProcessor
     }
 
     /**
+     * @return string
+     */
+    protected function getSaltedPassword($password)
+    {
+        $saltedPassword = $password;
+        if (ExtensionManagementUtility::isLoaded('saltedpasswords')) {
+            if (SaltedPasswordsUtility::isUsageEnabled('FE')) {
+                $objSalt = SaltFactory::getSaltingInstance(NULL);
+                if (is_object($objSalt)) {
+                    $saltedPassword = $objSalt->getHashedPassword($password);
+                }
+            }
+        }
+        return $saltedPassword;
+    }
+
+    /**
      * @see http://php.net/manual/en/function.uniqid.php#94959
      * @return string
      */
-    protected function getUuid()
-    {
-        return sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
-            // 32 bits for "time_low"
-            mt_rand(0, 0xffff), mt_rand(0, 0xffff),
-
-            // 16 bits for "time_mid"
-            mt_rand(0, 0xffff),
-
-            // 16 bits for "time_hi_and_version",
-            // four most significant bits holds version number 4
-            mt_rand(0, 0x0fff) | 0x4000,
-
-            // 16 bits, 8 bits for "clk_seq_hi_res",
-            // 8 bits for "clk_seq_low",
-            // two most significant bits holds zero and one for variant DCE1.1
-            mt_rand(0, 0x3fff) | 0x8000,
-
-            // 48 bits for "node"
-            mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
-        );
-    }
+    //protected function getUuid()
+    //{
+    //    return sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+    //        // 32 bits for "time_low"
+    //        mt_rand(0, 0xffff), mt_rand(0, 0xffff),
+    //
+    //        // 16 bits for "time_mid"
+    //        mt_rand(0, 0xffff),
+    //
+    //        // 16 bits for "time_hi_and_version",
+    //        // four most significant bits holds version number 4
+    //        mt_rand(0, 0x0fff) | 0x4000,
+    //
+    //        // 16 bits, 8 bits for "clk_seq_hi_res",
+    //        // 8 bits for "clk_seq_low",
+    //        // two most significant bits holds zero and one for variant DCE1.1
+    //        mt_rand(0, 0x3fff) | 0x8000,
+    //
+    //        // 48 bits for "node"
+    //        mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
+    //    );
+    //}
 
 }
