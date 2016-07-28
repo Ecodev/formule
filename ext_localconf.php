3969b63b3204215eead618d98b49d27de2c3e060
Fabien Udriot <fabien.udriot@ecodev.ch>
2016-07-28 10:54:57 +0200

    [CGL] PSR-2 of ext_tables and ext_localconf

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,54 +1,54 @@
 <?php
 if (!defined('TYPO3_MODE')) {
-	die('Access denied.');
+    die('Access denied.');
 }
 
 $configuration = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['formule']);
 
 if (FALSE === isset($configuration['autoload_typoscript']) || TRUE === (bool)$configuration['autoload_typoscript']) {
 
-	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript(
-		'formule',
-		'constants',
-		'<INCLUDE_TYPOSCRIPT: source="FILE:EXT:formule/Configuration/TypoScript/constants.ts">'
-	);
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript(
+        'formule',
+        'constants',
+        '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:formule/Configuration/TypoScript/constants.ts">'
+    );
 
-	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript(
-		'formule',
-		'setup',
-		'<INCLUDE_TYPOSCRIPT: source="FILE:EXT:formule/Configuration/TypoScript/setup.ts">'
-	);
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript(
+        'formule',
+        'setup',
+        '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:formule/Configuration/TypoScript/setup.ts">'
+    );
 }
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-	'Fab.formule',
-	'Pi1',
-	array(
-		'Form' => 'show, submit, feedback',
+    'Fab.formule',
+    'Pi1',
+    array(
+        'Form' => 'show, submit, feedback',
 
-	),
-	// non-cacheable actions
-	array(
-		'Form' => 'show, submit, feedback',
+    ),
+    // non-cacheable actions
+    array(
+        'Form' => 'show, submit, feedback',
 
-	)
+    )
 );
 
 // Duplicate feature of EXT:messenger
 if (!\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('messenger')) {
 
-	// Override classes for the Object Manager
-	$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['TYPO3\CMS\Core\Mail\MailMessage'] = array(
-		'className' => 'Fab\Formule\Override\Core\Mail\MailMessage'
-	);
+    // Override classes for the Object Manager
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['TYPO3\CMS\Core\Mail\MailMessage'] = array(
+        'className' => 'Fab\Formule\Override\Core\Mail\MailMessage'
+    );
 
-	# Install PSR-0-compatible class autoloader for Markdown Library in Resources/PHP/Michelf
-	spl_autoload_register(function ($class) {
-		if (strpos($class, 'Michelf\Markdown') !== FALSE) {
-			require sprintf('%sResources/Private/PHP/Markdown/%s',
-				\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('formule'),
-				preg_replace('{\\\\|_(?!.*\\\\)}', DIRECTORY_SEPARATOR, ltrim($class, '\\')) . '.php'
-			);
-		}
-	});
+    # Install PSR-0-compatible class autoloader for Markdown Library in Resources/PHP/Michelf
+    spl_autoload_register(function ($class) {
+        if (strpos($class, 'Michelf\Markdown') !== FALSE) {
+            require sprintf('%sResources/Private/PHP/Markdown/%s',
+                \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('formule'),
+                preg_replace('{\\\\|_(?!.*\\\\)}', DIRECTORY_SEPARATOR, ltrim($class, '\\')) . '.php'
+            );
+        }
+    });
 }
--- ext_tables.php
+++ ext_tables.php
@@ -1,12 +1,12 @@
 <?php
 if (!defined('TYPO3_MODE')) {
-	die('Access denied.');
+    die('Access denied.');
 }
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-	'Fab.formule',
-	'Pi1',
-	'Formule'
+    'Fab.formule',
+    'Pi1',
+    'Formule'
 );
 
 /** @var \TYPO3\CMS\Extbase\Object\ObjectManager $objectManager */
@@ -18,26 +18,28 @@ $configuration = $configurationUtility->getCurrentConfiguration('formule');
 
 // Possible Static TS loading
 if (TRUE === isset($configuration['autoload_typoscript']['value']) && FALSE === (bool)$configuration['autoload_typoscript']['value']) {
-	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('formule', 'Configuration/TypoScript', 'Variety of forms - effortless!');
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('formule', 'Configuration/TypoScript', 'Variety of forms - effortless!');
 }
 
-if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('vidi')) {
+if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('vidi')
+    && !\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('messenger')
+) {
 
-	/** @var \Fab\Vidi\Module\ModuleLoader $moduleLoader */
-	$moduleLoader = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\Fab\Vidi\Module\ModuleLoader::class, 'tx_formule_domain_model_sentmessage');
+    /** @var \Fab\Vidi\Module\ModuleLoader $moduleLoader */
+    $moduleLoader = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\Fab\Vidi\Module\ModuleLoader::class, 'tx_formule_domain_model_sentmessage');
 
-	/** @var \Fab\Vidi\Module\ModuleLoader $moduleLoader */
-	$moduleLoader->setIcon('EXT:formule/Resources/Public/Images/tx_formule_domain_model_sentmessage.png')
-		->setModuleLanguageFile('LLL:EXT:formule/Resources/Private/Language/tx_formule_domain_model_sentmessage.xlf')
-		->setDefaultPid(0)
-		->register();
+    /** @var \Fab\Vidi\Module\ModuleLoader $moduleLoader */
+    $moduleLoader->setIcon('EXT:formule/Resources/Public/Images/tx_formule_domain_model_sentmessage.png')
+        ->setModuleLanguageFile('LLL:EXT:formule/Resources/Private/Language/tx_formule_domain_model_sentmessage.xlf')
+        ->setDefaultPid(0)
+        ->register();
 }
 
 // Add Flexform
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['formule_pi1'] = 'pi_flexform';
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
-	'formule_pi1',
-	sprintf('FILE:EXT:formule/Configuration/FlexForm/Formule.xml')
+    'formule_pi1',
+    sprintf('FILE:EXT:formule/Configuration/FlexForm/Formule.xml')
 );
 
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['formule_pi1'] = 'layout, select_key, pages, recursive';
