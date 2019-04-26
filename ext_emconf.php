4099d1d3efd825bb77a0c552f3f0ae7ff647be54
Fabien Udriot <fabien@omic.ch>
2019-04-26 14:55:45 +0200

    Release 2.1.1 - <https://github.com/Ecodev/formule/compare/2.1.0...2.1.1>
    [BUGFIX] Fix wrong configuration utility call

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,7 +7,7 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Udriot Fabien',
     'author_email' => 'fabien@ecodev.ch',
     'state' => 'stable',
-    'version' => '2.2.0-dev',
+    'version' => '2.1.1',
     'psr-4' => [
         'Fab\\Formule\\' => 'Classes'
     ],
