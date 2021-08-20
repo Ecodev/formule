4efbef50c4dc3a8d7f7c91790809e9ff30564f08
Fabien Udriot <fabien@omic.ch>
2021-08-20 10:51:23 +0200

    [TASK] Prepare next release

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,7 +7,7 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Udriot Fabien',
     'author_email' => 'fabien@ecodev.ch',
     'state' => 'stable',
-    'version' => '3.0.1',
+    'version' => '3.0.1-dev',
     'psr-4' => [
         'Fab\\Formule\\' => 'Classes'
     ],
